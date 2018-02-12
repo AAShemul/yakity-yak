06c12e929c96fcfe96a754e142ee5570a4a63088
sloumdrone <brian.michael.evans@gmail.com>
2018-02-12 12:48:17 -0800

    Fixed a bunch of display issues on windows computers, rewrote almost all of the functions to make them more flexbile for dealing with any number of windows.

--- resources/signin.js
+++ resources/signin.js
@@ -7,20 +7,20 @@ var iconSelected = false;
 var homeOpen = false;
 
 function init(){
-    $('.square:nth-child(4)').on('click',minimize);
-    $('.open-app').on('click',minimize);
-    $('.square:nth-child(3)').on('click',maximize);
+    $('.square:nth-child(4)').on('click',minimizeWindow);
+    $('.open-app').on('click',barAppClick);
+    $('.square:nth-child(3)').on('click',maximizeWindow);
     $('.begin-menu li').on('click',setWallpaper);
     $('.begin-button').on('click',openBeginMenu);
     $(document).on('click',deselect);
     $('.desktop-icon p').on('click',highlightIcon);
-    $('.chat, .home').on('dblclick',openApp);
+    $('.desktop-icon').on('dblclick',openApp);
     $('.square:nth-child(2)').on('click',closeWindow);
     $('.container , .home-container').on('mousedown',focusWindow);
     $('.color').on('click',selectColor);
     $('.submit').on('click',enterChat);
 
-    $('.container, .home-container').draggable({
+    $('.container').draggable({
         cancel: '.container div, .container div *, .home-container div, .home-container div *'
     });
 
@@ -32,43 +32,22 @@ function init(){
         $('body').addClass(localStorage.getItem('background'));
     }
 
+    if (localStorage.getItem('background') || localStorage.getItem('userName') || localStorage.getItem('color')){
+        fillOutConfig();
+    }
+
     timeNow()
 
     var timeKeeper = setInterval(timeNow,30000);
 }
 
-function minimize(){
-    let obj = $(this).attr('class').split(' ')[0]
-    let app;
-
-    if (obj === 'square'){
-        app = $(this).parent().attr('class').split(' ')[1]
-        let otherapp = app === 'chatapp' ? 'homeapp' : 'chatapp';
-        $(this).parent().hide().css('z-index','10');
-        $('.open-app.'+app).addClass('close-app');
-        if ($('#desktop > .'+ otherapp).css('display') !== 'none'){
-            $('.open-app.'+otherapp).removeClass('close-app').css('z-index','20');
-        }
-    } else {
-        app = $(this).attr('class').split(' ')[1];
-        let otherapp = app === 'chatapp' ? 'homeapp' : 'chatapp';
-        if ($(this).hasClass('close-app')){
-            $('#desktop > .'+app).show().css('z-index','20');
-            $(this).removeClass('close-app');
-            $('#desktop > .'+ otherapp).css('z-index','10');
-            $('.open-app.'+otherapp).addClass('close-app');
-        } else {
-            $('#desktop > .'+app).hide().css('z-index','10');
-            $('#desktop > .'+ otherapp).css('z-index','20');
-            $(this).addClass('close-app');
-            if ($('#desktop > .'+ otherapp).css('display') !== 'none'){
-                $('.open-app.'+otherapp).removeClass('close-app');
-            }
-        }
-    }
+function minimizeWindow(){
+    let app = $(this).parent().attr('class').split(' ')[1];
+    $('.container.'+app).hide().css('z-index','10');
+    $('.open-app.'+app).addClass('close-app');
 }
 
-function maximize(){
+function maximizeWindow(){
     app = $(this).parent().attr('class').split(' ')[1];
 
     if (fs) {
@@ -90,11 +69,11 @@ function enterChat(){
         $('.mainbody').hide();
         $('iframe').attr('src','./main-chat.html');
         $('.chatbody').show();
+
+        fillOutConfig();
     } else {
-        $('.warning').text('User name must be at least 3 characters');
+        $('.warning').css('color','red').text('User name must be at least 3 characters');
     }
-
-
 }
 
 function selectColor(){
@@ -103,7 +82,6 @@ function selectColor(){
     let color = $(this).attr('class').split(' ')[1];
     localStorage.setItem('color',color);
 }
-
 
 function timeNow(){
     let d = new Date();
@@ -122,17 +100,17 @@ function timeNow(){
     $('.time-box span').text(`${hour}:${min}${ampm}`);
 }
 
-
 function setWallpaper(){
     let newClass = $(this).text();
     $('body').attr('class','');
     $('body').addClass(newClass);
-    console.log(newClass);
     localStorage.setItem('background',newClass);
+    fillOutConfig();
 }
 
 function openBeginMenu(event){
     event.stopPropagation();
+    console.log('begin click');
     $('.begin-menu').toggle();
 }
 
@@ -148,7 +126,7 @@ function highlightIcon(event){
     }
     event.stopPropagation();
     let icon = $(event.target).parent().attr('class').split(' ')[1];
-    $('.' + icon + ' p').addClass('highlighted');
+    $('.desktop-icon.' + icon + ' p').addClass('highlighted');
     iconSelected = true;
 }
 
@@ -158,54 +136,49 @@ function closeWindow(){
         $('.mainbody').show();
         $('iframe').attr('src','');
         $('.chatbody').hide();
-        $('.open-app.chatapp').hide();
-        chatOpen = false;
-    } else {
-        $('.open-app.homeapp').hide();
-        homeOpen = false;
     }
+    $('.open-app').addClass('close-app');
+    $('.open-app.'+app).hide();
     $(this).parent().hide();
 }
 
 function openApp(){
     let app = $(this).attr('class').split(' ')[1];
-    console.log(app);
-    if (app === 'chat'){
-        if (!chatOpen){
-            chatOpen = true;
-            $('.container').attr('style','');
-            $('.home-container').css('z-index','10');
-            $('.container').css('z-index','20');
-            $('.open-app.chatapp').css('display','inline-block');
-            $('.open-app.chatapp').removeClass('close-app');
-            $('.open-app.homeapp').addClass('close-app');
-        }
-    } else {
-        if (!homeOpen){
-            homeOpen = true;
-            $('.home-container').attr('style','');
-            $('.container').css('z-index','10');
-            $('.home-container').css('z-index','20');
-            $('.home-container.homeapp').show();
-            $('.open-app.homeapp').css('display','inline-block');
-            $('.open-app.chatapp').addClass('close-app');
-            $('.open-app.homeapp').removeClass('close-app');
-        }
-    }
+    $('.container.'+app).show();
+    $('.container').css('z-index','10');
+    $('.container.'+app).css('z-index','20');
+    $('.open-app').addClass('close-app');
+    $('.open-app.'+app).css('display','inline-block').removeClass('close-app');
 }
 
 function focusWindow(){
-    let target = $(this).attr('class').split(' ')[0];
-    console.log(target);
-    if (target === 'container'){
-        $('.container').css('z-index','20');
-        $('.home-container').css('z-index','10');
-        $('.open-app.chatapp').removeClass('close-app');
-        $('.open-app.homeapp').addClass('close-app');
-    } else {
+    let target = $(this).attr('class').split(' ')[1];
+    $('.container').css('z-index','10');
+    $('.container.'+target).css('z-index','20');
+    $('.open-app').addClass('close-app');
+    $('.open-app.'+target).removeClass('close-app');
+}
+
+function barAppClick() {
+    let app = $(this).attr('class').split(' ')[1];
+    if ($('.open-app.'+app).hasClass('close-app')){
+        $('.container.'+app).show();
         $('.container').css('z-index','10');
-        $('.home-container').css('z-index','20');
-        $('.open-app.chatapp').addClass('close-app');
-        $('.open-app.homeapp').removeClass('close-app');
+        $('.container.'+app).css('z-index','20');
+        $('.open-app').addClass('close-app');
+        $('.open-app.'+app).css('display','inline-block').removeClass('close-app');
+    } else {
+        $('.container.'+app).hide().css('z-index','10');
+        $('.open-app.'+app).addClass('close-app');
     }
+}
+
+
+function fillOutConfig(){
+    var $ul = $('<ul>');
+    var $username = $('<li>').text('User Name: '+(localStorage.getItem('userName') || '[None Set]'));
+    var $color = $('<li>').text('User Color: '+(localStorage.getItem('color') || '[None Set]'));
+    var $bg = $('<li>').text('User Background: '+(localStorage.getItem('background') || '[None Set]'));
+    $ul = $ul.append($username,$color,$bg);
+    $('#userinfo').empty().append($ul,'<p>Username and color can be changed in the chat app. Background can be changed in the "Begin" menu.</p>');
 }
